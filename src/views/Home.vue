1a6af69284c4124c899c2e48c115f250410ed6f4
Patrik Švikruha <patrik.svikruha@gmail.com>
2021-12-17 00:14:21 +0100

    add vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import NavigationLink from '@/components/slots/NavigationLink.vue'
+import TodoListItem from '@/components/slots/TodoListItem.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        HelloWorld,
+        NavigationLink,
+        TodoListItem
+    }
+}
+</script>
